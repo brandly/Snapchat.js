691c59ff3dc2bb9def9c92d41d723792d2d0994c
Matthew Brandly <brandly13@gmail.com>
2013-10-29 23:07:33 -0400

    starting to shove in a big canvas

--- snapchat.js
+++ snapchat.js
@@ -4,15 +4,14 @@
 
   var
   canvas = d.createElement('canvas'),
-  context = canvas.getContext('2d');
-
-  canvas.width = 10;
+  context = canvas.getContext('2d'),
+  pencil = d.createElement('div'),
+  currentColor = [0, 0, 0];
 
   function createVerticalRainbow(height) {
     // thanks dude
     // https://gist.github.com/robtarr/1199770
     var gradient = context.createLinearGradient(0, 0, 0, height);
-
     gradient.addColorStop(0,    "rgb(255,   0,   0)");
     gradient.addColorStop(0.15, "rgb(255,   0, 255)");
     gradient.addColorStop(0.33, "rgb(0,     0, 255)");
@@ -20,7 +19,6 @@
     gradient.addColorStop(0.67, "rgb(0,   255,   0)");
     gradient.addColorStop(0.84, "rgb(255, 255,   0)");
     gradient.addColorStop(1,    "rgb(255,   0,   0)");
-
     return gradient;
   }
 
@@ -33,8 +31,8 @@
     var canvasY = 0;
 
     do{
-        totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
-        totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+      totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+      totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
     }
     while(currentElement = currentElement.offsetParent)
 
@@ -44,15 +42,56 @@
     return {x:canvasX, y:canvasY}
   }
 
+  function applyStyles(element, styles) {
+    for (var item in styles) {
+      element.style[item] = styles[item];
+    }
+  }
+  applyStyles(pencil, {
+    width: 100,
+    height: 100,
+    border: '1px solid black'
+  });
+
+  canvas.width = 10;
   context.fillStyle = createVerticalRainbow(canvas.height);
   context.fillRect(0, 0, canvas.width, canvas.height);
 
-  canvas.onmousedown = function(e) {
-    var coords = relativeMouseCoords(e, canvas);
-    var data = context.getImageData(coords.x, coords.y, 1, 1).data;
-    d.body.style.backgroundColor= 'rgb(' + data[0] + ',' + data[1] + ',' + data[2] + ')';
+  function setDrawingColor(e) {
+    var
+    coords = relativeMouseCoords(e, canvas),
+    data = context.getImageData(coords.x, coords.y, 1, 1).data;
+    currentColor = [data[0], data[1], data[2]];
+    pencil.style.backgroundColor= 'rgb(' + currentColor.join(',') + ')';
   };
 
+  canvas.onmousedown = function(e) {
+    setDrawingColor(e);
+    canvas.onmousemove = setDrawingColor;
+    w.onmouseup = function(e) {
+      canvas.onmousemove = w.onmouseup = undefined;
+    };
+  };
+
+  pencil.onclick = function(e) {
+    canvas.style.display = (canvas.style.display === 'none') ? 'block' : 'none';
+  };
+
+  canvas.style.display = 'none';
+
+  var
+  bigCanvas = d.createElement('canvas'),
+  bigContext = bigCanvas.getContext('2d');
+
+  bigCanvas.width = w.innerWidth;
+  bigCanvas.height = w.innerHeight;
+
+  bigCanvas.onmousemove = function(e) {
+
+  };
+
+  d.body.appendChild(pencil);
   d.body.appendChild(canvas);
+  d.body.appendChild(bigCanvas);
 
 }).call(this, window, document);
