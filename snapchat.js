c36b367b048dfdd58735941349b6d6a52e4a0272
brandly <brandly13@gmail.com>
2014-02-23 01:40:38 -0500

    init

---
+++ snapchat.js
@@ -0,0 +1,158 @@
+
+(function(w, d) {
+  'use strict';
+
+  var
+  colorPicker = d.createElement('canvas'),
+  context = colorPicker.getContext('2d'),
+  pencil = d.createElement('div'),
+
+  config = {
+
+  },
+
+  currentColor = [255, 0, 0];
+  currentColor.toString = function() {
+    return 'rgb(' + currentColor.join(',') + ')';
+  };
+  currentColor.set = function (data) {
+    for (var i = 0; i <3 ; i++) {
+      currentColor[i] = data[i];
+    }
+  };
+
+  function createVerticalRainbow(height) {
+    // thanks dude
+    // https://gist.github.com/robtarr/1199770
+    var gradient = context.createLinearGradient(0, 0, 0, height);
+    gradient.addColorStop(0,    "rgb(255,   0,   0)");
+    gradient.addColorStop(0.15, "rgb(255,   0, 255)");
+    gradient.addColorStop(0.33, "rgb(0,     0, 255)");
+    gradient.addColorStop(0.49, "rgb(0,   255, 255)");
+    gradient.addColorStop(0.67, "rgb(0,   255,   0)");
+    gradient.addColorStop(0.84, "rgb(255, 255,   0)");
+    gradient.addColorStop(1,    "rgb(255,   0,   0)");
+    return gradient;
+  }
+
+  function relativeMouseCoords(event, currentElement) {
+    // thanks dude
+    // http://stackoverflow.com/questions/55677/how-do-i-get-the-coordinates-of-a-mouse-click-on-a-canvas-element
+    var totalOffsetX = 0;
+    var totalOffsetY = 0;
+    var canvasX = 0;
+    var canvasY = 0;
+
+    do{
+      totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+      totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+    }
+    while(currentElement = currentElement.offsetParent)
+
+    canvasX = event.pageX - totalOffsetX;
+    canvasY = event.pageY - totalOffsetY;
+
+    return {x:canvasX, y:canvasY}
+  }
+
+  function applyStyles(element, styles) {
+    for (var item in styles) {
+      element.style[item] = styles[item];
+    }
+  }
+
+  colorPicker.width = 15;
+  colorPicker.height = 200;
+  context.fillStyle = createVerticalRainbow(colorPicker.height);
+  context.fillRect(0, 0, colorPicker.width, colorPicker.height);
+
+  function setDrawingColor(e) {
+    var
+    coords = relativeMouseCoords(e, colorPicker);
+    currentColor.set( context.getImageData(0, Math.min(coords.y, colorPicker.height - 1), 1, 1).data );
+    pencil.style.backgroundColor = currentColor.toString();
+  };
+
+  colorPicker.onmousedown = function(e) {
+    setDrawingColor(e);
+    colorPicker.onmousemove = setDrawingColor;
+    colorPicker.onmouseout = colorPicker.onmouseup = function(e) {
+      colorPicker.onmousemove = colorPicker.onmouseout = colorPicker.onmouseup = undefined;
+    };
+  };
+
+  pencil.onclick = function(e) {
+    colorPicker.style.display = (colorPicker.style.display === 'none') ? '' : 'none';
+  };
+
+  colorPicker.style.display = 'none';
+
+  var
+  bigCanvas = d.createElement('canvas'),
+  bigContext = bigCanvas.getContext('2d');
+
+  bigCanvas.width = w.innerWidth;
+  bigCanvas.height = w.innerHeight;
+
+  var last = null;
+  function draw(element, coords) {
+    bigContext.beginPath();
+    bigContext.strokeStyle = currentColor.toString();
+    bigContext.lineWidth = 5;
+    bigContext.lineJoin = 'round';
+    bigContext.moveTo(last.x, last.y);
+    bigContext.lineTo(coords.x, coords.y);
+    bigContext.closePath();
+    bigContext.stroke();
+  }
+
+  bigCanvas.onmousedown = function(e) {
+    last = relativeMouseCoords(e, bigCanvas);
+
+    bigCanvas.onmousemove = function(e) {
+      var coords = relativeMouseCoords(e, bigCanvas);
+      draw(bigCanvas, coords);
+      last = relativeMouseCoords(e, bigCanvas);
+    };
+    bigCanvas.onmouseup = function(e) {
+      bigCanvas.onmousemove = bigCanvas.onmouseup = undefined;
+    };
+  };
+
+  applyStyles(bigCanvas, {
+    position: 'absolute',
+    top: 0,
+    left: 0
+  });
+
+  applyStyles(pencil, {
+    width: 60,
+    height: 60,
+    border: '1px solid black',
+    position: 'absolute',
+    top: '10px',
+    right: '10px'
+  });
+
+  applyStyles(colorPicker, {
+    position: 'absolute',
+    top: '95px',
+    right: '10px'
+  });
+
+  var
+  borderStyles = {
+    border: '4px solid white',
+    borderRadius: '5px',
+    'box-shadow': '0 0 5px black'
+  };
+  applyStyles(colorPicker, borderStyles);
+  applyStyles(pencil, borderStyles);
+
+  pencil.style.backgroundColor = currentColor.toString();
+
+  d.body.appendChild(bigCanvas);
+  d.body.appendChild(pencil);
+  d.body.appendChild(colorPicker);
+
+}).call(this, window, document);
