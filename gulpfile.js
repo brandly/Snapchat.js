8d37d3a3d2b4119eb081ea72a1eeb6232af783b7
brandly <brandly13@gmail.com>
2014-02-24 02:25:59 -0500

    uglify into gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,19 @@ var
 gulp = require('gulp'),
 gutil = require('gulp-util'),
 path = require('path'),
-express = require('express');
+express = require('express'),
+uglify = require('gulp-uglify'),
+concat = require('gulp-concat');
 
 gulp.task('gh', function () {
+    var dest = 'gh-pages/';
     gulp.src('src/*')
-        .pipe(gulp.dest('gh-pages/'));
+        .pipe(gulp.dest(dest));
+
+    gulp.src('src/snapchat.js')
+        .pipe(uglify())
+        .pipe(concat('snapchat.min.js'))
+        .pipe(gulp.dest(dest));
 });
 
 gulp.task('default', function () {
