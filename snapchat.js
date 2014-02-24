f0b8966f4305876b18e8090593c91e855f3b62df
brandly <brandly13@gmail.com>
2014-02-24 00:51:08 -0500

    undo

--- snapchat.js
+++ snapchat.js
@@ -3,182 +3,310 @@
   'use strict';
 
   var
-  colorPicker = d.createElement('canvas'),
-  context = colorPicker.getContext('2d'),
-  pencil = d.createElement('div'),
-
-  config = {
-
-  },
-
-  hasTouch = 'ontouchstart' in d.documentElement,
-  events = {
-    down: hasTouch ? 'touchstart' : 'mousedown',
-    move: hasTouch ? 'touchmove' : 'mousemove',
-    up: hasTouch ? 'touchend' : 'mouseup',
-    out: hasTouch ? 'touchcancel' : 'mouseout'
-  },
-
-  currentColor = [255, 0, 0];
-  currentColor.toString = function() {
-    return 'rgb(' + currentColor.join(',') + ')';
-  };
-  currentColor.set = function (data) {
-    for (var i = 0; i <3 ; i++) {
-      currentColor[i] = data[i];
-    }
-  };
-
-  function createVerticalRainbow(height) {
+  utils = {
+    hasTouch: 'ontouchstart' in d.documentElement,
     // thanks dude
     // https://gist.github.com/robtarr/1199770
-    var gradient = context.createLinearGradient(0, 0, 0, height);
-    gradient.addColorStop(0,    "rgb(255,   0,   0)");
-    gradient.addColorStop(0.15, "rgb(255,   0, 255)");
-    gradient.addColorStop(0.33, "rgb(0,     0, 255)");
-    gradient.addColorStop(0.49, "rgb(0,   255, 255)");
-    gradient.addColorStop(0.67, "rgb(0,   255,   0)");
-    gradient.addColorStop(0.84, "rgb(255, 255,   0)");
-    gradient.addColorStop(1,    "rgb(255,   0,   0)");
-    return gradient;
-  }
-
-  function relativeMouseCoords(event, currentElement) {
+    createVerticalRainbow: function(context, height) {
+      var gradient = context.createLinearGradient(0, 0, 0, height);
+      gradient.addColorStop(0,    "rgb(255,   0,   0)");
+      gradient.addColorStop(0.15, "rgb(255,   0, 255)");
+      gradient.addColorStop(0.33, "rgb(0,     0, 255)");
+      gradient.addColorStop(0.49, "rgb(0,   255, 255)");
+      gradient.addColorStop(0.67, "rgb(0,   255,   0)");
+      gradient.addColorStop(0.84, "rgb(255, 255,   0)");
+      gradient.addColorStop(1,    "rgb(255,   0,   0)");
+      return gradient;
+    },
     // thanks dude
     // http://stackoverflow.com/questions/55677/how-do-i-get-the-coordinates-of-a-mouse-click-on-a-canvas-element
-    var totalOffsetX = 0;
-    var totalOffsetY = 0;
-    var canvasX = 0;
-    var canvasY = 0;
+    relativeMouseCoords: function(event, el) {
+      var totalOffsetX = 0,
+          totalOffsetY = 0;
 
-    do{
-      totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
-      totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+      do {
+        totalOffsetX += el.offsetLeft - el.scrollLeft;
+        totalOffsetY += el.offsetTop - el.scrollTop;
+      } while(el = el.offsetParent);
+
+      return {
+        x: event.pageX - totalOffsetX,
+        y: event.pageY - totalOffsetY
+      }
+    },
+    css: function(element, styles) {
+      for (var item in styles) {
+        element.style[item] = styles[item];
+      }
+    },
+    draw: function(context, start, end) {
+      context.beginPath();
+      context.moveTo(start.x, start.y);
+      context.lineTo(end.x, end.y);
+      context.closePath();
+      context.stroke();
+    },
+    borderStyles: {
+      border: '4px solid white',
+      borderRadius: '5px',
+      'box-shadow': '0 0 5px black'
     }
-    while(currentElement = currentElement.offsetParent)
+  },
+  events = {
+    down: utils.hasTouch ? 'touchstart' : 'mousedown',
+    move: utils.hasTouch ? 'touchmove' : 'mousemove',
+    up: utils.hasTouch ? 'touchend' : 'mouseup',
+    out: utils.hasTouch ? 'touchcancel' : 'mouseout'
+  },
+  colorPicker = {
+    element: d.createElement('canvas'),
+    size: {
+      width: 15,
+      height: 200
+    },
+    init: function() {
+      var context, el = colorPicker.element;
+      colorPicker.context = context = el.getContext('2d');
 
-    canvasX = event.pageX - totalOffsetX;
-    canvasY = event.pageY - totalOffsetY;
+      // size
+      el.width = this.size.width;
+      el.height = this.size.height;
 
-    return {x:canvasX, y:canvasY}
-  }
+      // rainbow
+      this.context.fillStyle = utils.createVerticalRainbow(context, this.size.height);
+      this.context.fillRect(0, 0, this.size.width, this.size.height);
 
-  function applyStyles(element, styles) {
-    for (var item in styles) {
-      element.style[item] = styles[item];
+      // events
+      el.addEventListener(events.down, function(e) {
+        colorPicker.setDrawingColor(e);
+        function removeListeners(e) {
+          el.removeEventListener(events.move, colorPicker.setDrawingColor);
+          el.removeEventListener(events.up, removeListeners);
+          el.removeEventListener(events.out, removeListeners);
+        };
+        el.addEventListener(events.move, colorPicker.setDrawingColor);
+        el.addEventListener(events.up, removeListeners);
+        el.addEventListener(events.out, removeListeners);
+      });
+
+      // styles
+      utils.css(el, {
+        position: 'absolute',
+        top: '95px',
+        right: '10px',
+        zIndex: '1001',
+        display: 'none'
+      });
+      utils.css(el, utils.borderStyles);
+
+      // dom
+      d.body.appendChild(el);
+    },
+    setDrawingColor: function(e) {
+      var
+      coords = utils.relativeMouseCoords(e, colorPicker.element),
+      color = colorPicker.context.getImageData(0, Math.min(coords.y, colorPicker.size.height - 1), 1, 1).data;
+      pencil.setCurrentColor(color);
+    },
+    isVisibile: function() {
+      colorPicker.element.style.display === '';
+    },
+    show: function() {
+      utils.css(colorPicker.element, {display: ''});
+    },
+    hide: function() {
+      utils.css(colorPicker.element, {display: 'none'});
     }
-  }
+  },
+  pencil = {
+    element: d.createElement('div'),
+    init: function() {
+      var el = pencil.element;
 
-  colorPicker.width = 15;
-  colorPicker.height = 200;
-  context.fillStyle = createVerticalRainbow(colorPicker.height);
-  context.fillRect(0, 0, colorPicker.width, colorPicker.height);
+      // events
+      el.addEventListener('click', function(e) {
+        if (colorPicker.isVisibile()) {
+          colorPicker.hide();
+          bigCanvas.disable();
+        } else {
+          colorPicker.show();
+          bigCanvas.enable();
+        }
+      });
 
-  function setDrawingColor(e) {
-    var
-    coords = relativeMouseCoords(e, colorPicker);
-    currentColor.set( context.getImageData(0, Math.min(coords.y, colorPicker.height - 1), 1, 1).data );
-    pencil.style.backgroundColor = currentColor.toString();
-  }
+      // styles
+      utils.css(el, {
+        width: '60px',
+        height: '60px',
+        position: 'absolute',
+        top: '10px',
+        right: '10px',
+        zIndex: '1001'
+      });
+      utils.css(el, utils.borderStyles);
+      pencil.refresh();
 
-  colorPicker.addEventListener(events.down, function(e) {
-    setDrawingColor(e);
-    function removeListeners(e) {
-      colorPicker.removeEventListener(events.move, setDrawingColor);
-      colorPicker.removeEventListener(events.up, removeListeners);
-      colorPicker.removeEventListener(events.out, removeListeners);
-    };
-    colorPicker.addEventListener(events.move, setDrawingColor);
-    colorPicker.addEventListener(events.up, removeListeners);
-    colorPicker.addEventListener(events.out, removeListeners);
-  });
+      // dom
+      d.body.appendChild(el);
+    },
 
-  pencil.addEventListener('click', function(e) {
-    if (colorPicker.style.display === 'none') {
-      applyStyles(colorPicker, {display: ''});
-      applyStyles(bigCanvas, {'pointer-events': ''});
-    } else {
-      applyStyles(colorPicker, {display: 'none'});
-      applyStyles(bigCanvas, {'pointer-events': 'none'});
+    currentColor: [255, 0, 0],
+    getCurrentColor: function() {
+      return 'rgb(' + pencil.currentColor.join(',') + ')';
+    },
+    setCurrentColor: function(data) {
+      for (var i = 0; i <3 ; i++) {
+        pencil.currentColor[i] = data[i];
+      }
+      pencil.refresh();
+    },
+    refresh: function() {
+      var currentColor = pencil.getCurrentColor();
+      utils.css(pencil.element, {
+          backgroundColor: currentColor
+      });
+      bigCanvas.setColor(currentColor);
     }
-  });
+  },
+  bigCanvas = {
+    element: d.createElement('canvas'),
+    history: [],
+    init: function() {
+      var context, el = bigCanvas.element;
+      bigCanvas.context = context = this.element.getContext('2d');
 
-  colorPicker.style.display = 'none';
+      // size
+      el.width = w.innerWidth;
+      el.height = w.innerHeight;
 
-  var
-  bigCanvas = d.createElement('canvas'),
-  bigContext = bigCanvas.getContext('2d');
+      // events
+      var previousCoords = null;
+      el.addEventListener(events.down, function(e) {
+        // coords for a line
+        var line = [];
+        line.color = pencil.getCurrentColor();
 
-  bigCanvas.width = w.innerWidth;
-  bigCanvas.height = w.innerHeight;
+        function drawMove(e) {
+          var coords = utils.relativeMouseCoords(e, el);
+          utils.draw(context, previousCoords, coords);
+          previousCoords = coords;
+          line.push(coords);
+        }
 
-  var last = null;
-  function draw(element, coords) {
-    bigContext.beginPath();
-    bigContext.strokeStyle = currentColor.toString();
-    bigContext.lineWidth = 5;
-    bigContext.lineJoin = 'round';
-    bigContext.moveTo(last.x, last.y);
-    bigContext.lineTo(coords.x, coords.y);
-    bigContext.closePath();
-    bigContext.stroke();
-  }
+        function removeListeners(e) {
+          el.removeEventListener(events.move, drawMove);
+          el.removeEventListener(events.up, removeListeners);
+          bigCanvas.history.push(line);
+          undo.refresh();
+        }
 
-  bigCanvas.addEventListener(events.down, function(e) {
-    function drawMove(e) {
-      var coords = relativeMouseCoords(e, bigCanvas);
-      draw(bigCanvas, coords);
-      last = relativeMouseCoords(e, bigCanvas);
+        previousCoords = utils.relativeMouseCoords(e, el);
+        line.push(previousCoords);
+
+        el.addEventListener(events.move, drawMove);
+        el.addEventListener(events.up, removeListeners);
+      });
+
+      // style
+      utils.css(el, {
+        zIndex: '1000',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        'pointer-events': 'none',
+      });
+      bigCanvas.defaultLineStyle();
+
+      // dom
+      d.body.appendChild(el);
+    },
+    defaultLineStyle: function() {
+      bigCanvas.context.lineWidth = 5;
+      bigCanvas.context.lineJoin = 'round';
+    },
+    setColor: function(color) {
+      bigCanvas.context.strokeStyle = color;
+    },
+    enable: function() {
+      utils.css(bigCanvas.element, {'pointer-events': ''});
+    },
+    disable: function() {
+      utils.css(bigCanvas.element, {'pointer-events': 'none'});
+    },
+    redraw: function() {
+      // clear it
+      bigCanvas.element.width = bigCanvas.element.width;
+      bigCanvas.defaultLineStyle();
+
+      for(var i = 0, length = bigCanvas.history.length; i < length; i++) {
+        var line = bigCanvas.history[i];
+        bigCanvas.setColor(line.color);
+        for (var j = 0, max = line.length - 1; j < max; j++) {
+          utils.draw(bigCanvas.context, line[j], line[j + 1]);
+        };
+      }
+
+      // since we messed with bigCanvas' color
+      pencil.refresh();
+    },
+    undo: function() {
+      bigCanvas.history.pop();
+      bigCanvas.redraw();
+      undo.refresh();
     }
+  },
+  undo = {
+    element: d.createElement('canvas'),
+    size: {
+      width: 15,
+      height: 200
+    },
+    init: function() {
+      var context, el = undo.element;
+      undo.context = context = el.getContext('2d');
 
-    function removeListeners(e) {
-      bigCanvas.removeEventListener(events.move, drawMove);
-      bigCanvas.removeEventListener(events.up, removeListeners);
+      // events
+      el.addEventListener('click', function(e) {
+        bigCanvas.undo();
+      });
+
+      // styles
+      utils.css(el, {
+        display: 'none',
+        width: '50px',
+        height: '50px',
+        position: 'absolute',
+        top: '10px',
+        right: '90px',
+        zIndex: '1001'
+      });
+      utils.css(el, utils.borderStyles);
+
+      // dom
+      d.body.appendChild(el);
+    },
+    show: function() {
+      utils.css(undo.element, {display: ''});
+    },
+    hide: function() {
+      utils.css(undo.element, {display: 'none'});
+    },
+    refresh: function() {
+      if (bigCanvas.history.length) {
+        var lastColor = bigCanvas.history[bigCanvas.history.length - 1].color;
+        utils.css(undo.element, {
+          backgroundColor: lastColor
+        });
+        undo.show();
+      } else {
+        undo.hide();
+      }
     }
-
-    last = relativeMouseCoords(e, bigCanvas);
-    bigCanvas.addEventListener(events.move, drawMove);
-    bigCanvas.addEventListener(events.up, removeListeners);
-  });
-
-  applyStyles(bigCanvas, {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    'pointer-events': 'none',
-    zIndex: '1000'
-  });
-
-  applyStyles(pencil, {
-    width: '60px',
-    height: '60px',
-    border: '1px solid black',
-    position: 'absolute',
-    top: '10px',
-    right: '10px',
-    zIndex: '1001'
-  });
-
-  applyStyles(colorPicker, {
-    position: 'absolute',
-    top: '95px',
-    right: '10px',
-    zIndex: '1001'
-  });
-
-  var
-  borderStyles = {
-    border: '4px solid white',
-    borderRadius: '5px',
-    'box-shadow': '0 0 5px black'
   };
-  applyStyles(colorPicker, borderStyles);
-  applyStyles(pencil, borderStyles);
 
-  pencil.style.backgroundColor = currentColor.toString();
-
-  d.body.appendChild(bigCanvas);
-  d.body.appendChild(pencil);
-  d.body.appendChild(colorPicker);
+  bigCanvas.init();
+  pencil.init();
+  colorPicker.init();
+  undo.init();
 
 }).call(this, window, document);
